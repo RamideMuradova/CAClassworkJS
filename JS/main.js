ba1f03ddbeb1d9a5e8af736a6e2e4bb832689e4f
RamideMuradova <ramidemuradova003@gmail.com>
2023-11-01 13:37:07 +0400

    JS

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+let temperature = 12;
+
+if (temperature > 22) {
+  console.log(`Nazik geyin`);
+} else if (temperature > 15) {
+  console.log(`Pencek geyin`);
+} else {
+  console.log(`Qalin geyin`);
+}
+
+let letter = "a";
+if (letter == "a" || letter == "i" || letter =="e" || letter =="o" || letter=="u"){
+    console.log(`${letter} letter is saitdir`);
+}
