9fadca9bbc45155467d27288297c445a1d244d94
RamideMuradova <ramidemuradova003@gmail.com>
2023-11-03 13:58:46 +0400

    03.ARRAY&OBJECT

--- 01.11 -JS/main.js
+++ 01.11 -JS/main.js
@@ -36,4 +36,4 @@ let factorial = 1;
 for (let i = 1; i <= n; i++) {
   factorial *= i;
 }
-
+console.log(`faktorial:${factorial}`);
---
+++ 03.ARRAY&OBJECT/app.js
@@ -0,0 +1,32 @@
+//TASK 1
+// let numbers = [12, 45, 2222, 78, 34, 99, 23];
+// let max = numbers[0];
+// let min = numbers[0];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > max) {
+//     max = numbers[i];
+//   }
+//   else if (numbers[i] < min) {
+//     min = numbers[i];
+//   }
+// }
+// console.log("MIN :", min);
+// console.log("MAX :", max);
+
+
+//TASK 2
+// let numbers =[12,45,6,78,34,99,23]
+// let sum =0;
+// for (let i = 0; i < numbers.length; i++) {
+//     sum +=numbers[i];
+
+// }
+// console.log("SUM :" ,sum);
+
+//TASK 3
+// const elements =[1,2,3,4,5];
+// const temp = elements [2];
+// elements[2] = elements[3];
+// elements[3] =temp;
+// console.log("ARRAY :" + elements);
