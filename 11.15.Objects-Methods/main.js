03ec101a7d22e4fac242285b371fc6cb9875ffbb
RamideMuradova <ramidemuradova003@gmail.com>
2023-11-15 13:55:17 +0400

    11.15.Objects-Methods/

---
+++ 11.15.Objects-Methods/main.js
@@ -0,0 +1,60 @@
+const person = {
+  firstName: "Ramide",
+  lastName: "Muradova",
+  age: "20",
+  country: "Azerbaijan",
+  university: "GDU",
+  city: "Goygol",
+};
+// person["phone number"] = 0556655545;
+// person.city = "Goygol";
+// console.log(person);
+// console.log(Object.keys(person));
+// console.log();
+
+// // TASK 1
+// // 1)
+// const employees = {
+//   boss: "Michael",
+//   secretary: "Pam",
+//   sales: "Jim",
+//   accountant: "Oscar",
+// };
+// console.log(Object.keys(employees));
+// //2)
+// console.log(Object.values(employees));
+// //3)
+// console.log(Object.entries(employees));
+// //4)
+// let arr = Object.values(employees);
+// let result = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (result.length < arr[i].length) {
+//     result = arr[i];
+//   }
+// }
+// console.log(result);
+//5)
+// delete person.age;
+// console.log(person);
+
+//TASK 2
+
+var library = [
+  {
+    title: "The Road Ahead",
+    author: "Bill Gates",
+    readingStatus: true,
+  },
+  {
+    title: "Walter Isaacson",
+    author: "Bill Gates",
+    readingStatus: true,
+  },
+  {
+    title: "Suzanne Collins",
+    author: "Mockingjay:The Final Book",
+    readingStatus: false,
+  },
+];
