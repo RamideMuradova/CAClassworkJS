4587e4ed3e7f7300f870fe83c5a5d16009f389a1
RamideMuradova <ramidemuradova003@gmail.com>
2023-11-21 13:52:55 +0400

    11.21.js-dom/

---
+++ 11.21.js-dom/main.js
@@ -0,0 +1,38 @@
+// TASK 1 //
+
+// let addBgColor = document.querySelector(".addBgColor");
+// addBgColor.addEventListener("click",function () {
+//     let parentElement=addBgColor.parentElement;
+//     parentElement.style.background = "green";
+//     parentElement.style.width = "200px";
+//     parentElement.style.height = "200px";
+//     parentElement.style.position = "relative";
+//     this.style.position = "absolute";
+//     this.style.bottom = "2px";
+//     this.style.right = "10px";
+// });
+
+// TASK 2 //
+// let ulElement = document.createElement("ul");
+// let cities = ["Ganja", "Baku", "Yeklakh", "Goranboy", "Shamkir"];
+// cities.forEach((element) => {
+//   let liElement = document.createElement("li");
+//   liElement.innerText = element;
+//   ulElement.append(liElement);
+//   liElement.addEventListener("click", function () {
+//     this.remove();
+//   });
+// });
+
+// document.querySelector(".cities").append(ulElement);
+
+// TASK 3//
+
+
+
+// TASK 4 //
+
+// let dark = document.querySelector(".dark");
+// dark.addEventListener("click",function(){
+//     document.body.classList.toggle("theme")
+// });
