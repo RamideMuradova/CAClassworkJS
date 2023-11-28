244ad1ca0e6209851b891730a9d460de6be1099e
RamideMuradova <ramidemuradova003@gmail.com>
2023-11-28 10:12:20 +0400

    task

--- 11.27.JS-storage/about.js
+++ 11.27.JS-storage/about.js
@@ -5,5 +5,15 @@ renderTable(userData);
 function renderTable(userData) {
   let table = document.querySelector("table");
   let tbody = document.querySelector("tbody");
-  
+
+  userData.forEach((data) => {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `<td>${data.id}</td>
+    <td>${data.firstName}</td>
+    <td>${data.lastName}</td>
+    <td>${data.address}</td>`;
+    tbody.append(tr);
+  });
+  //Table add html
+  table.appendChild(tbody);
 }
