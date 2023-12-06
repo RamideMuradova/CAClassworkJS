4fedf1981ee3086b305a0f13b6e80675e80bb083
Ramide Muradova <ramidemuradova003@gmail.com>
2023-12-06 01:41:28 -0800

    ./12.06.filter-project/

--- 12.05-API-practise/weather.js
+++ 12.05-API-practise/weather.js
@@ -1,10 +1,19 @@
-let curCity = "Goygol";
+let curCity = "baku";
+let country = document.querySelector(".country");
 
 function getForecast() {
   fetch(
     `https://api.weatherapi.com/v1/current.json?key=de4fcc1d06a64857be7101727222212&q=${curCity}&aqi=no`
   )
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then(
+      (data) =>
+        (dataWeahter.innerHTML = `<p>${data.location?.name}</p>
+      <img src="${data.current.condition.icon}"alt=""
+      <p>${data.current.condition.text}</p>
+      <p>${data.current.temp_c}</p>
+      <p>${data.current.temp_f}</p>
+      <p>${data.location.localtime_epoch}</p>`)
+    );
 }
-getForecast()
+getForecast();
---
+++ 12.06.filter-project/main.js
@@ -0,0 +1,42 @@
+const BASE_URL = "https://fakestoreapi.com/products";
+let allCards = document.querySelector(".allCards");
+let electroBtn = document.querySelector(".electroBtn");
+let jeweleryBtn = document.querySelector(".jewelery");
+let menClohets = document.querySelector(".men");
+let womenclohets = document.querySelector(".women");
+
+async function getAllData(endpoint) {
+  try {
+    const response = await axios(`${BASE_URL}/${endpoint}`);
+    console.log(response.data);
+    drawProducts(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getAllData("")
+function drawProducts(array) {
+    allCards.innerHTML =""
+ array.forEach((element) => {
+    allCards.innerHTML +=`
+    <div class="card">
+        <img src="${element.image}" alt="" />
+        <p>${element.description}</p>
+        <p>${element.price}</p>
+    </div>`;
+  });
+}
+
+
+electroBtn.addEventListener("click", function () {
+  getAllData("/category/electronics");
+});
+jeweleryBtn.addEventListener("click", function () {
+  getAllData("/category/jewelery");
+});
+menClohets.addEventListener("click", function () {
+  getAllData("/category/men's%20clothing");
+});
+womenclohets.addEventListener("click", function () {
+  getAllData("/category/women's%20clothing");
+});
