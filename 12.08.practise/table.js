b89ae51ce3d1584b847ce9d706b865188a1a52cf
Ramide Muradova <ramidemuradova003@gmail.com>
2023-12-08 01:15:13 -0800

    ./12.08.practise/

--- 12.08.practise/table.js
+++ 12.08.practise/table.js
@@ -1,16 +1,36 @@
 let tbody = document.querySelector("tbody");
-function drawTable(tableBody) {
+let search = document.querySelector(".search");
+let spinner =document.querySelector(".spinner")
+const BASE_URL = "http://universities.hipolabs.com";
+
+async function getAllData() {
+    spinner.classList.add("show")
+  const res = await axios(`${BASE_URL}/search?country=Azerbaijan`);
+  spinner.classList.remove("show")
+  drawTable(res.data);
+  console.log(res.data);
+}
+getAllData();
+
+function drawTable(array) {
   tbody.innerHTML = "";
-  tableBody.forEach((element) => {
+  array.forEach((element) => {
     let trElement = document.createElement("tr");
     trElement.innerHTML += `
-        <td>${element.university.name}</td>
+        <td>${element.name}</td>
         <td>${element.country} </td>
-        <td>${element.domaines}</td>
-        <td>${element.website}</td>
+        <td>${element.domains[0]}</td>
+        <td>${element.web_pages[0]}</td>
         `;
     tbody.append(trElement);
   });
 }
-drawTable(data);
-getAllData
+// drawTable(data);
+
+search.addEventListener("input", async function (event) {
+    spinner.classList.add("show")
+  const res = await axios(`${BASE_URL}/search?country=Azerbaijan&name=${event.target.value}`);
+  spinner.classList.remove("show")
+  drawTable(res.data);
+  console.log(res.data);
+});
