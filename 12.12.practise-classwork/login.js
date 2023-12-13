f83d22165750b521437730dac01a219ac3aada3f
Ramide Muradova <ramidemuradova003@gmail.com>
2023-12-12 20:47:46 -0800

    task

--- 12.12.practise-classwork/login.js
+++ 12.12.practise-classwork/login.js
@@ -1,6 +1,8 @@
 let form = document.querySelector("form");
 let allInput = document.querySelectorAll("input");
 let newArr = JSON.parse(localStorage.getItem("User")) || [];
+let icon = document.querySelector(".fa-eye");
+let icon2 = document.querySelector(".fa-eye-slash");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   let bool = newArr.find((item) => {
@@ -13,4 +15,14 @@ form.addEventListener("submit", function (e) {
   }else{
     alert("istifadeci tapilmadi")
   }
+});
+icon.addEventListener("click", function () {
+  allInput[1].type = "text";
+  this.style.display = "none";
+  icon2.style.display = "block";
+});
+icon2.addEventListener("click", function () {
+  allInput[1].type = "password";
+  this.style.display = "none";
+  icon.style.display = "block";
 });
