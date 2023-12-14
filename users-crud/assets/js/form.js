972d1cbacc766f5c23de5a0e4a8aa1752e1445a6
Ramide Muradova <ramidemuradova003@gmail.com>
2023-12-14 00:31:02 -0800

    task

--- users-crud/assets/js/form.js
+++ users-crud/assets/js/form.js
@@ -8,10 +8,5 @@ async function fillForm() {
   const res = await axios(`${BASE_URL}/users/${id}`);
   console.log(res.data);
 
-  allInputs[0].value = res.data.id;
-  allInputs[1].value = res.data.userphoto;
-  allInputs[2].value = res.data.name;
-  allInputs[3].value = res.data.surname;
-  allInputs[4].value = res.data.email;
-  allInputs[5].value = res.data.date;
+  
 }
